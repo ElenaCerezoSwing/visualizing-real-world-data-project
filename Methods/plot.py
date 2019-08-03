869aefe24f41c82c7ba770de102659cd366efacd
ElenaCerezoSwing <elenacerezo83@gmail.com>
2019-08-03 10:40:00 +0200

    adds more changes

--- Methods/plot.py
+++ Methods/plot.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+from seaborn import distplot
 
 def get_boxplot(df, col):
     df.boxplot(column=col)
@@ -12,8 +13,15 @@ def get_violinplot(df, col):
 def get_bar(df, col):
   df[col].value_counts().plot.bar()
 
+def get_horizontal_bar(df, col):
+  df[col].value_counts().plot.barh()
+
 def get_scatter(x, y, area, colors, opacity):
     plt.scatter(x, y, s=area, c=colors, alpha=opacity)
+    plt.show()
+
+def get_line(x, y, colors):
+    plt.plot(x, y,c=colors)
     plt.show()
 
 def get_hist(df, col):
@@ -21,3 +29,6 @@ def get_hist(df, col):
 
 def get_scatter_matrix(df):
     pd.plotting.scatter_matrix(df)
+
+def get_distplot(df, col, bool):
+    distplot(df[col], kde=bool)
